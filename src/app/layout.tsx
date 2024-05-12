4bcbaa27470017c919566954490796bf75731b61
Adli Ihsan Hariadi <11794203+adliih@users.noreply.github.com>
2024-05-12 13:58:56 +0700

    feat: add y padding on layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <main className="container mx-auto px-2">{children}</main>
+        <main className="container mx-auto p-2">{children}</main>
       </body>
       <PrelineScript />
     </html>
