b9b4275a3c51b3bb585adfc527a038fd71b37536
Adli Ihsan Hariadi <11794203+adliih@users.noreply.github.com>
2024-05-20 12:58:24 +0700

    feat: specify grid-rows for minimum row count on product list

--- src/app/components/ProductList.tsx
+++ src/app/components/ProductList.tsx
@@ -12,7 +12,7 @@ export default function ProductList({
   CardAdditionalComponents,
 }: Props) {
   return (
-    <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-5 xl:grid-cols-6 gap-2">
+    <div className="grid grid-cols-2 grid-rows-2 sm:grid-cols-3 sm:grid-rows-3 lg:grid-cols-5 lg:grid-rows-5 xl:grid-cols-6 xl:grid-rows-6 gap-2">
       {products.map((product) => (
         <ProductCard
           key={product.name}
