3c09312eddcdfdebe87f3b279713f90e91b0a8d0
Adli Ihsan Hariadi <11794203+adliih@users.noreply.github.com>
2024-05-23 10:58:51 +0700

    feat: add provider filter on homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,15 @@ export const dynamic = "force-dynamic"; // defaults to auto
 import { grouper, productFetcher } from "@/lib";
 import GroupedProductList from "./components/GroupedProductList";
 
-export default async function Home() {
-  const products = await productFetcher.get();
+interface Param {
+  searchParams: {
+    providers: string[];
+  };
+}
+
+export default async function Home({ searchParams }: Param) {
+  const { providers } = searchParams;
+  const products = await productFetcher.get({ filter: { providers } });
   const groups = grouper.byProvider(products);
 
   return <GroupedProductList groups={groups} />;
