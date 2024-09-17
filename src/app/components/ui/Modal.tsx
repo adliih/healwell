76466c02204c0bd929a06e228f57365d75836404
Adli Ihsan Hariadi <11794203+adliih@users.noreply.github.com>
2024-09-17 07:00:55 +0700

    feat: remove dark modifier on modal

--- src/app/components/ui/Modal.tsx
+++ src/app/components/ui/Modal.tsx
@@ -18,17 +18,17 @@ export default function Modal({
       aria-labelledby="hs-vertically-centered-modal-label"
     >
       <div className="hs-overlay-open:mt-7 hs-overlay-open:opacity-100 hs-overlay-open:duration-500 mt-0 opacity-0 ease-out transition-all sm:max-w-lg sm:w-full m-3 sm:mx-auto min-h-[calc(100%-3.5rem)] flex items-center">
-        <div className="w-full flex flex-col bg-white border shadow-sm rounded-xl pointer-events-auto dark:bg-neutral-800 dark:border-neutral-700 dark:shadow-neutral-700/70">
-          <div className="flex justify-between items-center py-3 px-4 border-b dark:border-neutral-700">
+        <div className="w-full flex flex-col bg-white border shadow-sm rounded-xl pointer-events-auto">
+          <div className="flex justify-between items-center py-3 px-4 border-b">
             <h3
               id="hs-vertically-centered-modal-label"
-              className="font-bold text-gray-800 dark:text-white"
+              className="font-bold text-gray-800"
             >
               {title}
             </h3>
             <button
               type="button"
-              className="size-8 inline-flex justify-center items-center gap-x-2 rounded-full border border-transparent bg-gray-100 text-gray-800 hover:bg-gray-200 focus:outline-none focus:bg-gray-200 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-700 dark:hover:bg-neutral-600 dark:text-neutral-400 dark:focus:bg-neutral-600"
+              className="size-8 inline-flex justify-center items-center gap-x-2 rounded-full border border-transparent bg-gray-100 text-gray-800 hover:bg-gray-200 focus:outline-none focus:bg-gray-200 disabled:opacity-50 disabled:pointer-events-none"
               aria-label="Close"
               data-hs-overlay={`#${id}`}
             >
@@ -51,10 +51,10 @@ export default function Modal({
             </button>
           </div>
           <div className="p-4 overflow-y-auto">{children}</div>
-          <div className="flex justify-end items-center gap-x-2 py-3 px-4 border-t dark:border-neutral-700">
+          <div className="flex justify-end items-center gap-x-2 py-3 px-4 border-t">
             <button
               type="button"
-              className="py-2 px-3 inline-flex items-center gap-x-2 text-sm font-medium rounded-lg border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 focus:outline-none focus:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-800 dark:border-neutral-700 dark:text-white dark:hover:bg-neutral-700 dark:focus:bg-neutral-700"
+              className="py-2 px-3 inline-flex items-center gap-x-2 text-sm font-medium rounded-lg border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 focus:outline-none focus:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none"
               data-hs-overlay={`#${id}`}
             >
               Close
