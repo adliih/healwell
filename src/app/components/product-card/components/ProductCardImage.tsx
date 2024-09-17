17ed6e274f1a619dab70b56cfd1a33dbc6e9f31b
Adli Ihsan Hariadi <11794203+adliih@users.noreply.github.com>
2024-09-17 07:28:31 +0700

    feat: use shadcn dialog instead of preline modal

--- src/app/components/product-card/components/ProductCardImage.tsx
+++ src/app/components/product-card/components/ProductCardImage.tsx
@@ -1,9 +1,17 @@
 import { ProductData } from "@/lib/type";
 import Image from "next/image";
-import ModalButton from "../../ui/ModalButton";
-import Modal from "../../ui/Modal";
 import ProductCardContainer from "./ProductCardContainer";
 import Carousel from "../../ui/Carousel";
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { Button } from "@/components/ui/button";
 
 export default function ProductCardImage({
   product,
@@ -14,30 +22,44 @@ export default function ProductCardImage({
   const imgUrl = imageUrls?.[0];
   return (
     <>
-      <Modal id={id} title={name}>
-        <Carousel
-          items={imageUrls.map((url) => (
-            <ProductCardContainer>
-              <div className="relative grow">
-                <Image
-                  className="w-full aspect-square rounded-t-xl object-contain"
-                  fill={true}
-                  src={url}
-                  alt={name}
-                />
-              </div>
-            </ProductCardContainer>
-          ))}
-        />
-      </Modal>
-      <ModalButton className="relative grow" modalId={id}>
-        <Image
-          className="w-full aspect-square rounded-t-xl object-cover"
-          fill={true}
-          src={imgUrl}
-          alt={name}
-        />
-      </ModalButton>
+      <Dialog>
+        <DialogTrigger asChild>
+          <div className="relative grow cursor-pointer">
+            <Image
+              className="w-full aspect-square rounded-t-xl object-cover"
+              fill={true}
+              src={imgUrl}
+              alt={name}
+            />
+          </div>
+        </DialogTrigger>
+        <DialogContent className="sm:max-w-[425px]">
+          <DialogHeader>
+            <DialogTitle>{name}</DialogTitle>
+          </DialogHeader>
+          <Carousel
+            items={imageUrls.map((url) => (
+              <ProductCardContainer>
+                <div className="relative grow">
+                  <Image
+                    className="w-full aspect-square rounded-t-xl object-contain"
+                    fill={true}
+                    src={url}
+                    alt={name}
+                  />
+                </div>
+              </ProductCardContainer>
+            ))}
+          />
+          <DialogFooter>
+            <DialogClose asChild>
+              <Button type="button" variant="secondary">
+                Close
+              </Button>
+            </DialogClose>
+          </DialogFooter>
+        </DialogContent>
+      </Dialog>
     </>
   );
 }
